944d10b2078d5e78be30431ded72832a91ab74eb
Drew Ferreira <drewferreira83@gmail.com>
2018-10-25 11:15:12 -0400

    Initial Commit

---
+++ ViciniT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViciniT
+//
+//  Created by Andrew Ferreira on 10/25/18.
+//  Copyright © 2018 Andrew Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
