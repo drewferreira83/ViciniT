e8efbc03b5f7543fe55e2fb2b015ac1cfdcb9189
Drew Ferreira <drewferreira83@gmail.com>
2018-12-19 08:51:33 -0500

    Combined Help and Settings view.

--- ViciniT/SettingsViewController.swift
+++ ViciniT/SettingsViewController.swift
@@ -8,12 +8,17 @@
 
 import UIKit
 import MapKit
+import WebKit
 
 class SettingsViewController: UIViewController {
     @IBOutlet weak var trackUserSwitch: UISwitch!
     @IBOutlet weak var subwayButton: UIButton!   // GTFS RouteType of 0 and 1
     @IBOutlet weak var commRailButton: UIButton!
     @IBOutlet weak var busButton: UIButton!
+    
+    
+    @IBOutlet weak var infoWebView: WKWebView!
+    
     
     var routeTypes = UserSettings.shared.routeTypes
     
@@ -40,6 +45,14 @@ class SettingsViewController: UIViewController {
         trackUserSwitch.isOn = UserSettings.shared.trackUser
         trackUserSwitch.isEnabled = CLLocationManager.locationServicesEnabled()
         updateImages()
+        
+        if let localFile = Bundle.main.url(forResource: "ViciniTHelp", withExtension: "rtf", subdirectory: nil, localization: nil) {
+            let myRequest = NSURLRequest(url: localFile) as URLRequest
+            infoWebView.load(myRequest)
+        } else {
+            Debug.log("Couldn't load Help file!", flag: .important )
+        }
+        
         super.viewDidLoad()
     }
     
@@ -51,16 +64,5 @@ class SettingsViewController: UIViewController {
         commRailButton.setImage( commRailImage, for: .normal)
         busButton.setImage(busImage, for: .normal)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
